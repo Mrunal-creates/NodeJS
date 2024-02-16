addb252a3218bec1039bc87dc66c40f65c4d109e
Mrunal-creates <119970042+Mrunal-creates@users.noreply.github.com>
2024-02-16 14:47:37 +0530

    Update file.cjs

--- assignement 1/requirement 2/file.cjs
+++ assignement 1/requirement 2/file.cjs
@@ -1,1 +1,16 @@
 
+
+function isLeapYear(year){
+    if((year%4 == 0 && year%100 !== 0 ) || (year%400 ===0)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+export default isLeapYear
+
+export {isLeapYear}
+// export {isLeapYear};
+// console.log(isLeapYear(2024));
