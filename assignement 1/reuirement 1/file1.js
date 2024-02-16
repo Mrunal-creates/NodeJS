f525d0fdc8ca02bc12b32ba1173275a60f5298eb
Mrunal-creates <119970042+Mrunal-creates@users.noreply.github.com>
2024-02-16 14:46:26 +0530

    Add files via upload

---
+++ assignement 1/reuirement 1/file1.js
@@ -0,0 +1,8 @@
+// Import the reusable function individually
+
+import { isLeapYear } from "./dateUtil.js";
+
+
+console.log(isLeapYear(2024)); //leap year test
+
+console.log(isLeapYear(2019)); //Non leap year test
---
+++ assignement 1/reuirement 1/file2.js
@@ -0,0 +1,7 @@
+//  import the module as a variable 
+
+import dateUtil from './dateUtil.js'
+
+console.log(dateUtil(2020));//leap
+
+console.log(dateUtil(2015));//non leap
